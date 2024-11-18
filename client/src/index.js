3ac95d5bc88f8c5b69113187dcb1ff5dfe873a10
Billy <billyLin1954@gmail.com>
2024-11-17 20:49:34 -0500

    g

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,9 @@ import ramen from './img/ramen.png'
 import novice from './img/novice.png'
 var clicks = 0;
 var ramenRate = 0;
-
+const bodystyle={
+  textAlign: "center",
+}
 function loadClicks() {
   const savedClicks = localStorage.getItem("clicks");
   const ramenrate= localStorage.getItem("ramenRate")
@@ -13,29 +15,35 @@ function loadClicks() {
   ramenRate = ramenrate ? JSON.parse(ramenrate) : 0;
   document.getElementById("ramen_count").innerHTML=clicks;
 }
+document.addEventListener("DOMContentLoaded", loadClicks);
+const sizestyle={
+  height:200 ,
+ width:200 
+}
 class Header extends React.Component {
   render() {
   
     
     return(
-      <body>
-        <div>
+      <body >
+        <div id="upgrades">
+          <button><img src={novice} alt="" style={sizestyle}class="upgrades" onclick="increaseRate()"></img> </button>
+          <br></br>
+          novice  
+          <br></br>100 ramen
+        </div>
+        <div style={bodystyle}>
          <h1>Ramen clicker</h1>
       <br/>
       <h1 id="count">ramen count:</h1>
       <br/>
       <h1 id="ramen_count">r</h1>
-      <img id="ramen" src={ramen}alt="ramen" onclick="clicker()"></img>
+      <img style={sizestyle} src={ramen}alt="ramen" onclick="clicker()"></img>
       <button onclick="click()" >press</button>
       <button onclick="remove()">reset</button>
     
-    <div id="upgrades">
-      <button><img src={novice} alt="" class="upgrades" onclick="increaseRate()"></img> </button>
-      novic
-      <br></br>100 ramen
     </div>
-    </div>
-      </body>
+          </body>
     
     
     )
@@ -48,7 +56,7 @@ class App extends React.Component {
       <div>
 
         <Header/>
-        <h1>hello world</h1>
+        
       </div>
   )
   }
