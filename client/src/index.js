9ecb9218be38bd5e026e12ae297521d376a8cb96
Billy <billyLin1954@gmail.com>
2024-11-17 19:32:14 -0500

    l

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,14 @@ class Header extends React.Component {
   render() {
   
     
+    return(<h1>hello world</h1>)
   }
 }
 class App extends React.Component {
-  
+  render(){f
+
+    return(<h1>hello world</h1>)
+  }
 }
 
 const rootElement = document.getElementById('root')
