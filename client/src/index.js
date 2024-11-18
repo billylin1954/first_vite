62a12d9cce8afe90e87022d67170cd0629654171
Billy <billyLin1954@gmail.com>
2024-11-17 19:29:00 -0500

    kill

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Header extends React.Component {
+  render() {
+  
+    
+  }
+}
+class App extends React.Component {
+  
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
